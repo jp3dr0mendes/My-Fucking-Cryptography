fa1b6bb22629162a14e70d21854819227c35759a
pedro <joao.pedro73210@gmail.com>
2022-05-01 20:40:44 -0300

    incluindo a lógica de captura dos valores de input no HTML via JS

---
+++ crypto.js
@@ -0,0 +1,15 @@
+function crypto() {
+    var user = document.getElementById("user")
+
+    var pass = document.getElementById("pass")
+
+    console.log(user.value)
+    console.log(pass.value)
+
+    //função de criar tabela usuário, id e senha
+    //função de aplicar criptografia a senha
+    //função de mostrar a senha cripstografada
+
+    user.value = ""
+    pass.value = ""
+}
